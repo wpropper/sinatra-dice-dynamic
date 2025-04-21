a7fafa38e69bfafac31087128da1f52f311003ae
wpropper <wpropper@chicagobooth.edu>
2025-04-21 00:15:38 +0000

    final

--- app.rb
+++ app.rb
@@ -52,3 +52,20 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+# /app.rb
+
+get("/dynamic/:number_of_dice/6") do
+  @num_dice = params.fetch("number_of_dice").to_i
+
+  @sides = params.fetch("how_many_sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
